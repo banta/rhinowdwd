e85dde4cf2abc44cde6b17165aad3838df5ea5b0
Bernard Banta <banta.bernard@gmail.com>
2013-07-08 23:16:00 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rhino::Application.config.secret_key_base = 'e0f31acf4770e86f40470728edd7657a893cc559fc586fb3555c59ea2abf9186b4d6eb140f8737756b2ce69ec7c769d636c26c31d32e2d485644e55532f9ac45'
